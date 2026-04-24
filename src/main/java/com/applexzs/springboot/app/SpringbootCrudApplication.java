9323e712c8a4c110924fedce1019fdbbf54a8d08
Applexzs <reverseapplemw2@gmail.com>
2026-04-24 16:50:53 -0600

    add custom error messages & a configuration class

--- src/main/java/com/applexzs/springboot/app/SpringbootCrudApplication.java
+++ src/main/java/com/applexzs/springboot/app/SpringbootCrudApplication.java
@@ -3,6 +3,7 @@ package com.applexzs.springboot.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringbootCrudApplication {
 
