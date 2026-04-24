f1b59edd643494357201be7c701c1bf27e10bb40
Applexzs <reverseapplemw2@gmail.com>
2026-04-24 14:16:02 -0600

    add product services

---
+++ src/main/java/com/applexzs/springboot/app/services/IProductService.java
@@ -0,0 +1,16 @@
+package com.applexzs.springboot.app.services;
+
+import com.applexzs.springboot.app.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    Optional<Product> findById(Long id);
+
+    Product save(Product product);
+
+    Optional<Product> delete(Product product);
+}
---
+++ src/main/java/com/applexzs/springboot/app/services/ProductServiceImpl.java
@@ -0,0 +1,46 @@
+package com.applexzs.springboot.app.services;
+
+import com.applexzs.springboot.app.entities.Product;
+import com.applexzs.springboot.app.repositories.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ProductServiceImpl implements IProductService{
+
+    @Autowired
+    private IProductRepository repository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Product> findAll() {
+        return (List<Product>) repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Product> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Product save(Product product) {
+        return repository.save(product);
+    }
+
+    @Transactional
+    @Override
+    public Optional<Product> delete(Product product) {
+        Optional<Product> productOptional = repository.findById(product.getId());
+        productOptional.ifPresent(productDb -> {
+            repository.delete(productDb);
+        });
+        return productOptional;
+    }
+}
