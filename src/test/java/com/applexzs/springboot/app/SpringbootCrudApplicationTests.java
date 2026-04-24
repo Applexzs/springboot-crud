905ca98a85b504c2955ce525bb8832907626bddd
Applexzs <reverseapplemw2@gmail.com>
2026-04-24 13:50:57 -0600

    Launch

---
+++ src/test/java/com/applexzs/springboot/app/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applexzs.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
