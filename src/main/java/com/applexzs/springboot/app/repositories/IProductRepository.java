e53a6bdc392185470b0421e84e6bcec397cf1cf1
Applexzs <reverseapplemw2@gmail.com>
2026-04-24 14:03:11 -0600

    add product repository

---
+++ src/main/java/com/applexzs/springboot/app/repositories/IProductRepository.java
@@ -0,0 +1,7 @@
+package com.applexzs.springboot.app.repositories;
+
+import com.applexzs.springboot.app.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IProductRepository extends CrudRepository<Product, Long> {
+}
